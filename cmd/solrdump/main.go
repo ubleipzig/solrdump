31043c86a41818883dbaabd173bce7ddd21dd158
Martin Czygan <martin.czygan@gmail.com>
2021-10-19 21:25:13 +0200

    v0.1.7

--- cmd/solrdump/main.go
+++ cmd/solrdump/main.go
@@ -13,7 +13,7 @@ import (
 )
 
 // Version of application.
-const Version = "0.1.6"
+const Version = "0.1.7"
 
 var (
 	server                      = flag.String("server", "http://localhost:8983/solr/example", "SOLR server, host post and collection")
